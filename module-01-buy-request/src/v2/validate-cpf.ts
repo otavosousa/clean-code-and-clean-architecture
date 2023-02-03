215f55d48e9e60749cebb11145e7d24a1d4a002c
otavosousa <otavioklearskin@gmail.com>
2023-02-03 07:39:58 -0300

    refactor: implement calculate method

--- module-01-buy-request/src/v2/validate-cpf.ts
+++ module-01-buy-request/src/v2/validate-cpf.ts
@@ -1,5 +1,7 @@
 const NUMBER_REFERENCE_CALCULATE_CHECK_DIGIT_1 = 11
 const NUMBER_REFERENCE_CALCULATE_CHECK_DIGIT_2 = 12
+const NUMBER_QUANTITY_CHARACTERS = 9
+const DIGIT_ZERO = 0
 
 function isNumberCharactersValid(cpf: string){
   return cpf.length >= 11 || cpf.length<= 14
@@ -21,28 +23,34 @@ function calculateCharacter(character: string, index: number, reference: number)
   return ( reference - (index + 1) ) * Number(character)
 }
 
-function calculateCheckDigit(calculatedCharacters: number){
-  const calculateRest = calculatedCharacters % 11
-  return calculateRest < 2 ? 0 : 11 - calculateRest
-}
-
-function calculateCharacters(cpf: string[], reference: number): number{
-  return cpf.reduce((result, character, index) => {
+function calculateCheckDigit(cpf: string[]): string{
+  const reference = cpf.length === NUMBER_QUANTITY_CHARACTERS ? NUMBER_REFERENCE_CALCULATE_CHECK_DIGIT_1 : NUMBER_REFERENCE_CALCULATE_CHECK_DIGIT_2
+  const calculatedCharacters = cpf.reduce((result, character, index) => {
     return result + calculateCharacter(character, index, reference)
   }, 0)
+  const calculatedRest = calculatedCharacters % 11
+  return String(calculatedRest < 2 ? DIGIT_ZERO : 11 - calculatedRest)
 }
 
-export const validateCpf = function(cpf: string){
+function removeCheckDigits(cpf: string){
+  return cpf.split('').slice(0, -2)
+}
+
+function addCheckDigit(cpf: string[], checkDigit: string){
+  return [...cpf, checkDigit]
+}
+
+export const validateCpf = function(cpf: string | any){
   if(!cpf) return false
   if(!isNumberCharactersValid(cpf)) return false
   cpf = sanitizeCpf(cpf)
   if(isAllCharactersEquals(cpf)) return false
-  const cpfWithoutCheckDigits = cpf.split('').slice(0, -2)
-  const calculateCharactersCheckDigit1 = calculateCharacters(cpfWithoutCheckDigits, NUMBER_REFERENCE_CALCULATE_CHECK_DIGIT_1)
-  const checkDigit1 = calculateCheckDigit(calculateCharactersCheckDigit1)
-  const calculateCharactersCheckDigit2 = calculateCharacters(cpfWithoutCheckDigits, NUMBER_REFERENCE_CALCULATE_CHECK_DIGIT_2) + 2 * checkDigit1
-  const checkDigit2 = calculateCheckDigit(calculateCharactersCheckDigit2)
-  const isValidCpf = cpf.slice(-2) === `${checkDigit1}${checkDigit2}`
+  const checkDigits = cpf.slice(-2)
+  cpf = removeCheckDigits(cpf)
+  const checkDigit1 = calculateCheckDigit(cpf)
+  cpf = addCheckDigit(cpf, checkDigit1)
+  const checkDigit2 = calculateCheckDigit(cpf)
+  const isValidCpf = checkDigits === `${checkDigit1}${checkDigit2}`
   return isValidCpf
 }
 
